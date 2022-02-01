d1caeb5f20952b720831d7217a0c3904c8ff5bdf
Lord-Giganticus <mrubins64@gmail.com>
2022-02-01 09:36:19 -0600

    Add 2 c exports

--- source/Export.cpp
+++ source/Export.cpp
@@ -57,3 +57,13 @@ EXPORT bool LoadXML(char* XMLPath, char* BMGName, char* TBLName, bool LittleEndi
     auto writer = BmgWriter(XMLPath, BMGName, TBLName);
     return writer.writeBmg(LittleEndian);
 }
+
+extern "C" bool BMGToXML(char* BMGPath, char* TBLPath) {
+    BMGINFO info = BMGINFO(LoadBMG(BMGPath), LoadBCSV(TBLPath));
+    return BMGINFO::Write(info, "Message.xml");
+}
+
+extern "C" bool XMLToBMG(char* XMLPath, bool LittleEndian) {
+    auto writer = BmgWriter(XMLPath, "Message.bmg", "MessageId.tbl");
+    return writer.writeBmg(LittleEndian);
+}
